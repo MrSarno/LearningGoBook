2b1f24996ca76cd81950107a7435cfbc59d07509
MrSarno <email@sarno.dev>
2022-05-04 10:40:11 +0100

    add: Chap1 Prog3

---
+++ Chapter1/03-printcmdargs2.go
@@ -0,0 +1,16 @@
+// This program prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
