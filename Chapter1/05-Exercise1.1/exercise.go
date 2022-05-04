46a1cc3b91852ef119edc8950a0e1cdf04bc75bf
MrSarno <email@sarno.dev>
2022-05-04 11:30:15 +0100

    add: exercise 1.1

---
+++ Chapter1/05-Exercise1.1/exercise.go
@@ -0,0 +1,13 @@
+// This program prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(os.Args[0])
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
