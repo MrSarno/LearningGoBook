d3d0d0cb18ffcfea84f4f587b42fc92c675a57b4
MrSarno <email@sarno.dev>
2022-05-11 23:56:33 +0100

    add: DuplicateLines

---
+++ Chapter1/08-DuplicateLines/duplicates.go
@@ -0,0 +1,22 @@
+// This program prints lines that appear more than once.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
